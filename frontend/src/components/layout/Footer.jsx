be1682518e39fd173339ae11372a4fd3283b7029
Roshan Kumar Chellagonda <roshan@Roshans-MacBook-Air.local>
2026-07-04 04:37:21 +0530

    Initial commit

---
+++ frontend/src/components/layout/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p className="text-center">
+          Food Delivery Website - 2026, All Rights Reserved..
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
